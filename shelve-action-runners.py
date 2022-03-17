f144ce366486349a06233d1eb8272000db388079
Joshua A. Anderson <joaander@umich.edu>
2022-03-17 13:05:44 -0400

    Add script to shelve action runners.

---
+++ shelve-action-runners.py
@@ -0,0 +1,43 @@
+"""Shelve powered down actions runner instances in jetstream2."""
+
+import openstack
+import sys
+import time
+
+
+def shelve_runners(connection):
+    """Shelve runners that are powered down."""
+    try:
+        servers = list(connection.compute.servers())
+    except Exception as e:
+        print('::warning:: Failed to enumerate servers:', str(e))
+        return False
+
+    for server in servers:
+        if server.name.startswith('actions-runner'):
+
+            print(
+                f'Server {server.name} is {server.status}({server.task_state}).'
+            )
+            if server.status == 'SHUTOFF' and server.task_state is None:
+                print(f'... shelving {server.name}.')
+
+                try:
+                    connection.compute.shelve_server(server)
+                except Exception as e:
+                    print(f'::warning:: Failed to shelve server {server.name}:',
+                          str(e))
+
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    # catch errors and return success so that this script doesn't stop the whole
+    # actions job
+    try:
+        connection = openstack.connect()
+    except Exception as e:
+        print('::warning:: Failed to connect to cloud:', str(e))
+        sys.exit(0)
+
+    shelve_runners(connection)
