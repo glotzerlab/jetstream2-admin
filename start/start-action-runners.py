d21171fed96b5a3d1937190fda7caf8c27d1d01a
Joshua A. Anderson <joaander@umich.edu>
2023-04-11 10:49:00 -0400

    Bump copyright year.

--- start/start-action-runners.py
+++ start/start-action-runners.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022 The Regents of the University of Michigan.
+# Copyright (c) 2022-2023 The Regents of the University of Michigan.
 # Part of the Glotzerlab jetstream2 administration scripts, released under the
 # BSD 3-Clause License.
 
