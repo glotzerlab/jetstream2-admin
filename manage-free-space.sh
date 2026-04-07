a199de2da0e374adbc443758b88fb51499e5af72
Joshua A. Anderson <joaander@umich.edu>
2026-04-07 16:11:31 -0400

    Prune only docker images.

--- manage-free-space.sh
+++ manage-free-space.sh
@@ -5,10 +5,10 @@ free_space=$(stat -fc '%f * %S' / | bc)
 
 echo $(date): Free disk space: ${free_space} bytes.
 
-if (( $free_space < 10737418240 )); then
+if (( $free_space < 25769803776 )); then
     echo "... delete log files."
     journalctl --vacuum-size=200M
 
     echo "... prune docker images."
-    docker system prune -a -f
+    docker image prune -a -f
 fi
